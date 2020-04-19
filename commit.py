496b996a6880c904215f38badffe1dd3553ecbb8
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-04-19 10:09:44 +0530

    time.sleep scheduling

--- commit.py
+++ commit.py
@@ -1,17 +1,13 @@
 import subprocess
 import write
 import random
-from apscheduler.schedulers.blocking import BlockingScheduler
+import time
 
-sched = BlockingScheduler()
-
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=10)
-def scheduled_job():
+while True:
+    time.sleep(86400)
     for _ in range(random.randint(1,3)):
         text = random.randint(0,99999)
         write.write("file.txt",text)
         subprocess.run('git add file.txt', shell=True)
         subprocess.run('git commit -m "new commit"', shell=True)
         subprocess.run('git push', shell=True)
-
-sched.start()
